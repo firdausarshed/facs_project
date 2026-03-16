c8c83585db752963aabd2fa2326bd63e59ddbb5f
firdausarshed <firdausarshed33@gmail.com>
2026-03-16 17:44:10 +0000

    all au data processed successfully. stored in au_processed_data.

---
+++ data/to_jpeg.py
@@ -0,0 +1,13 @@
+#converting pngs to jpeg to overcome openface file restrictions
+
+from PIL import Image
+import os
+
+src = r"C:\Users\fifia\PycharmProjects\facs_project\data\jaffe_png"
+dst = r"C:\Users\fifia\PycharmProjects\facs_project\data\jaffe_jpg"
+os.makedirs(dst, exist_ok=True)
+
+for f in os.listdir(src):
+    if f.endswith('.png'):
+        img = Image.open(os.path.join(src, f)).convert('RGB')
+        img.save(os.path.join(dst, f.replace('.png', '.jpg')))
